f3b5f1d0f380a8002bed48705ca2a26f7d33d39b
GrupoTCC3h3 <157658093+viniciusN444@users.noreply.github.com>
2026-04-12 12:29:45 -0300

    Dado o inicio ao script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('#novoServico');
+
+btn.addEventListener('click', ()=>{
+    const addServico = createElement('div');
+    addServico.classList.add("novoServ");
+
+    const labelNome = document.createElement('label');
+    labelNome.textContent = "Nome:"
+    const txtNome = document.createElement('input');
+    
+
+})
